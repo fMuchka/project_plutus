5cd8536e647532ff3bd5860c3adfd5fae8d829ce
Ajvo8 <ivo.zeleny@gmail.com>
2022-12-09 07:16:17 +0100

    F data store + tests

--- src/data/Store.ts
+++ src/data/Store.ts
@@ -5,6 +5,7 @@ import type { Fee } from './records/Fee';
 import type { PortfolioItem } from './records/PortfolioItem';
 import type { Data } from './providers/Data';
 import type {Currencies } from './records/Currencies';
+import type { RecordBase } from './records/RecordBase';
 
 export class Store extends Dexie {
 
@@ -27,22 +28,72 @@ export class Store extends Dexie {
         this.sells.clear();
     }
 
-    loadDividends(dividends: Dividend[]) {
-        for (const dividend of dividends) {
-            this.dividends.add(dividend);
-        }
+    async loadBuys(items: PortfolioItem[]) {
+        await this.transaction("rw", this.buys, () => {
+            for (const item of items) {
+                this.fixAmount(item);
+                this.buys.add(item);
+            }
+        });
+    }
+
+    async loadSells(items: PortfolioItem[]) {
+        await this.transaction("rw", this.sells, () => {
+            for (const item of items) {
+                this.fixAmount(item);
+                this.sells.add(item);
+            }
+        });
+    }
+
+    fixAmount(item: PortfolioItem){
+        item.amount = item.pricePerShare * item.quantity;
+    }
+
+    async loadDividends(dividends: Dividend[]) {
+        await this.transaction("rw", this.dividends, () => {
+            for (const dividend of dividends) {
+                this.dividends.add(dividend);
+            }
+        });
     }
 
     async groupDividendsByCurrencyAndYear(year: number) : Promise<{[key in Currencies]? : number}> {
         let sumByCurrency: {[key in Currencies]? : number} = {};
 
         await this.dividends.each(d => {
-          if (d.timestamp.getFullYear() == year) {
+          if (this.matchYear(d,year)) {
             sumByCurrency[d.currency] = (sumByCurrency[d.currency] || 0) + d.amount;
           }
         });
 
         return sumByCurrency;
+    }
+
+    async getPortfolio() : Promise<{[key in string] : number}> {
+        let sumByTicker: {[key in string]: number} = {};
+
+        await this.buys.each(d => {
+          sumByTicker[d.ticker] = (sumByTicker[d.ticker] || 0) + d.quantity;
+        });
+
+        return sumByTicker;
+    }
+
+    async getSellsAmount(year: number) : Promise<number> {
+        let sum = 0;
+
+        await this.sells.each(d => {
+            if (this.matchYear(d,year)) {
+                sum += d.amount;
+            }
+        });
+
+        return sum;
+    }
+
+    matchYear(record: RecordBase, year: number){
+        return record.timestamp.getFullYear() == year;
     }
 }
 
--- src/data/__tests__/Store.test.ts
+++ src/data/__tests__/Store.test.ts
@@ -7,7 +7,8 @@ describe('Store tests', () => {
   it('sum of dividends paid in USD', async () => {
     const store = new Store();
     
-    store.loadDividends([
+    await store.loadDividends([
+      {currency: Currencies.USD, amount: 3, sourceId: "", timestamp: new Date('2021-12-31'),ticker: ""},
       {currency: Currencies.USD, amount: 1, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
       {currency: Currencies.USD, amount: 2, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""}
     ]);
@@ -21,7 +22,7 @@ describe('Store tests', () => {
     const store = new Store();
     store.clearAll();
     
-    store.loadDividends([
+    await store.loadDividends([
       {currency: Currencies.USD, amount: 1, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
       {currency: Currencies.USD, amount: 2, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
       {currency: Currencies.EUR, amount: 3, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
@@ -34,21 +35,34 @@ describe('Store tests', () => {
     expect(dividendsByCurrency[Currencies.EUR]).toBe(7);
   });
 
-  it('sum of dividends paid in USD and EUR', async () => {
+  it('sum of portfolio after 2 buys of same stock', async () => {
     const store = new Store();
     store.clearAll();
     
-    store.loadDividends([
-      {currency: Currencies.USD, amount: 1, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
-      {currency: Currencies.USD, amount: 2, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
-      {currency: Currencies.EUR, amount: 3, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""},
-      {currency: Currencies.EUR, amount: 4, sourceId: "", timestamp: new Date('2022-11-03'),ticker: ""}
+    await store.loadBuys([
+      {currency: Currencies.USD, quantity: 1, sourceId: "", timestamp: new Date('2022-01-01'),ticker: "AAPL", pricePerShare: 3, amount: 3},
+      {currency: Currencies.USD, quantity: 2, sourceId: "", timestamp: new Date('2022-01-02'),ticker: "AAPL", pricePerShare: 4, amount: 8}
     ]);
 
-    const dividendsByCurrency = await store.groupDividendsByCurrencyAndYear(2022);
+    const portfolio = await store.getPortfolio();
 
-    expect(dividendsByCurrency[Currencies.USD]).toBe(3);
-    expect(dividendsByCurrency[Currencies.EUR]).toBe(7);
+    expect(Object.keys(portfolio).length).toBe(1);
+    expect(portfolio["AAPL"]).toBe(3);
+  });
+
+  it('taxable amount after 2 sells in 2022', async () => {
+    const store = new Store();
+    store.clearAll();
+    
+    await store.loadSells([
+      {currency: Currencies.USD, quantity: 3, sourceId: "", timestamp: new Date('2021-12-31'),ticker: "O", pricePerShare: 5, amount: 15},
+      {currency: Currencies.USD, quantity: 1, sourceId: "", timestamp: new Date('2022-01-01'),ticker: "AAPL", pricePerShare: 3, amount: 3},
+      {currency: Currencies.USD, quantity: 2, sourceId: "", timestamp: new Date('2022-01-02'),ticker: "MSFT", pricePerShare: 4, amount: 8}
+    ]);
+
+    const sumOfSells = await store.getSellsAmount(2022);
+
+    expect(sumOfSells).toBe(11);
   });
 })
 
